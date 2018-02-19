0ee7b79d50c7725bbe8e9a8478ad72c50a0fb5e5
Christian <christian.curilla@publicispixelpark.de>
2018-02-19 16:05:43 +0100

    Example scenario

--- CompositeAndIterator/Program.cs
+++ CompositeAndIterator/Program.cs
@@ -10,6 +10,27 @@ namespace CompositeAndIterator
     {
         static void Main(string[] args)
         {
+            var C1 = new Composite("C 1");
+
+            var C11 = new Composite("C 1.1");
+            var C12 = new Composite("C 1.2");
+
+            var L111 = new Leaf("L 1.1.1");
+            var C121 = new Composite("C 1.2.1");
+            var L122 = new Leaf("L 1.2.2");
+
+            var L1211 = new Leaf("L 1.2.1.1");
+
+            C121.Add(L1211);
+
+            C12.Add(C121);
+            C12.Add(L122);
+
+            C11.Add(L111);
+
+            C1.Add(C11);
+            C1.Add(C12);
+
 
         }
     }
